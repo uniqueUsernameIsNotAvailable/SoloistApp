fc09df8fee0cf51840eaebf1a6884038bc0fa43b
uniqueUsernameIsNotAvailable <33angle@bk.ru>
2025-02-16 16:52:58 +0300

    fix for ui component compat

--- app/src/main/java/com/testchamber/soloistapp/core/ui/components/MusicSearchBar.kt
+++ app/src/main/java/com/testchamber/soloistapp/core/ui/components/MusicSearchBar.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun SearchBar(
+fun MusicSearchBar(
     searchQuery: String,
     onSearchQueryChange: (String) -> Unit,
     modifier: Modifier = Modifier,
